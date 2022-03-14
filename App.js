d9fe02e381280e7a1476ee6160f50eea31bf3b36
Shah Jahan <95079845+Shah-Jahan-Mukhtar@users.noreply.github.com>
2022-03-14 14:02:45 -0700

    CRUD Operations

--- App.js
+++ App.js
@@ -1,20 +1,104 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
+import Data from "./Component/Data";
 
 export default function App() {
+  const [create, setCreate] = useState({
+    name: "",
+    rollNo: "",
+  });
+  const [user, setUser] = useState([]);
+
+  const CreateData = () => {
+    setCreate([...user, create]);
+    // setCreate(null);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ marginTop: 25 }}>
+      <View style={styles.container}>
+        <View style={{ justifyContent: "center", alignItems: "center" }}>
+          <TextInput
+            style={styles.TextInput}
+            placeholder="Name"
+            value={create.name}
+            onChangeText={(text) => setCreate({ ...user, name: text })}
+          />
+        </View>
+        <View style={{ justifyContent: "center", alignItems: "center" }}>
+          <TextInput
+            style={styles.TextInput}
+            placeholder="Password"
+            keyboardType="number-pad"
+            value={create.rollNo}
+            onChangeText={(text) => setCreate({ ...user, rollNo: text })}
+            // secureTextEntry={true}
+          />
+        </View>
+        <View style={{ flexDirection: "row" }}>
+          <TouchableOpacity style={styles.Button} onPress={CreateData}>
+            <Text>Add</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.Button}>
+            <Text>Delete</Text>
+          </TouchableOpacity>
+        </View>
+        <View
+          style={{
+            alignItems: "center",
+            justifyContent: "center",
+            marginTop: 20,
+          }}
+        >
+          <View
+            style={{
+              width: 300,
+              height: 450,
+              backgroundColor: "#00bfff",
+
+              borderRadius: 5,
+            }}
+          >
+            <Text>Name:{user.name}</Text>
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // flex: 1,
+    backgroundColor: "#fff",
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  TextInput: {
+    width: 300,
+    height: 50,
+    marginTop: 20,
+    padding: 10,
+    elevation: 5,
+    shadowOpacity: 20,
+    borderRadius: 5,
+  },
+  Button: {
+    width: 70,
+    height: 45,
+    marginTop: 20,
+    marginLeft: 20,
+    backgroundColor: "#1E90FF",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 5,
   },
 });
